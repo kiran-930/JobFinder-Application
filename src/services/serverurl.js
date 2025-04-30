b27e1982943ce8952c474d29fc72e8c639c8517e
kiran-930 <kiranperiye@gmail.com>
2025-04-30 12:21:40 +0530

    updated

--- src/services/serverurl.js
+++ src/services/serverurl.js
@@ -1,7 +1,10 @@
-const SERVERURL = "https://jobfinder-server-1.onrender.com"
+const SERVERURL = "https://project-server-1-bd87.onrender.com"
 
 export default SERVERURL
 
 //https://project-fair-server-hnp6.onrender.com
 
 //http://localhost:4005
+
+
+//https://jobfinder-server-1.onrender.com
